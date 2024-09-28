4e76cb955b2de184f85e62938a2570b74e08eed5
Miron Jakubowski <mijakubowski@gmail.com>
2024-09-28 19:36:17 +0200

    Convert project to api

--- SocialBridge.Main/Program.cs
+++ SocialBridge.Main/Program.cs
@@ -1,41 +1,23 @@
 using SocialBridge.Main.Infrastructure;
 
-const string CorsPolicy = "AllowLocalhost3000";
-
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
-services.AddCors(options =>
-{
-    options.AddPolicy(CorsPolicy,
-        b => b.WithOrigins("http://localhost:3000")
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-});
-
 // Add services to the container.
-services.AddRazorPages();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 DiContainerConfig.AddDatabase(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseCors(CorsPolicy);
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapGet("/", () => "Hello from World");
 
 app.Run();
